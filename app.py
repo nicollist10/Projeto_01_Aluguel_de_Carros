9e46202c848a055205a842f84d3a3219c5fdc3ac
nicollist10 <teixeiranicolli2009@gmail.com>
2026-05-27 13:40:09 -0300

    primeira versão

--- app.py
+++ app.py
@@ -1,48 +1,12 @@
 import streamlit as st
-st.title("M motors - Aluguel")
-st.sidebar.title("Escolha seu modelo:")
+st.title('top multimarcas')
+st.subheader('“Mais do que dirigir um carro de luxo, aqui você vive uma experiência exclusiva, onde elegância, potência e sofisticação se encontram para transformar cada trajeto em um momento inesquecível.”')
+st.subheader('“Cada detalhe foi pensado para quem não aceita o comum. Descubra uma nova forma de dirigir, com carros que unem luxo, tecnologia, conforto e desempenho em uma experiência criada para impressionar em todos os momentos.”')
+st.sidebar.title('Seu próximo carro de luxo está aqui.')
 st.sidebar.image('logo.png')
-
-
-carros = ['BMW','Mustang', 'Porsche', 'Fusca', 'Toro']
-
-
-
-opcao = st.sidebar.selectbox('Escolha o carro que foi alugado', carros)
-
-
+carros = ['BMW', 'AUDI', 'FERRARI', 'MERCEDES', 'lamborghini', 'BUGATTI','MCLAREN']
+opcao = st.sidebar.selectbox('escolha o carro que foi alugado ou comprado', carros)
 
 st.image(f'{opcao}.png')
-st.markdown(f'## Você alugou o modelo: {opcao}')
+st.markdown(f'##Você acaba de escolher excelência sobre rodas c: {opcao}')
 st.markdown('---')
-
-dias = st.text_input(f'Por quantos dias o {opcao} foi alugado?')
-km = st.text_input(f'Quantos km você rodou com o {opcao}?')
-
-if opcao == 'BMW':
-    diaria = 450
-
-elif opcao == 'Mustang':
-    diaria = 500
-
-elif opcao == 'Porsche':
-    diaria = 300
-
-elif opcao == 'Fusca':
-    diaria = 250
-
-elif opcao == 'Toro':
-    diaria = 550
-
-
-if st.button('Calcular'):
-    dias = int(dias)
-    km = float(km)
-
-    total_dias = dias * diaria
-    total_km = km * 0.15
-    aluguel_total = total_dias+total_km
-
-    st.warning(f'Você alugou o {opcao} por {dias} dias e rodou {km}km. O valor total a pagar é R${aluguel_total:.2f}')
-
-
