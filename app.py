702a957fea8b6c932a54969f0fa988d6538175c7
nicollist10 <teixeiranicolli2009@gmail.com>
2026-05-27 16:34:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.subheader('“Mais do que dirigir um carro de luxo, aqui você vive uma exper
 st.subheader('“Cada detalhe foi pensado para quem não aceita o comum. Descubra uma nova forma de dirigir, com carros que unem luxo, tecnologia, conforto e desempenho em uma experiência criada para impressionar em todos os momentos.”')
 st.sidebar.title('Seu próximo carro de luxo está aqui.')
 st.sidebar.image('logo.png')
-carros = ['BMW', 'AUDI', 'FERRARI', 'MERCEDES', 'lamborghini', 'BUGATTI','MCLAREN']
+carros = ['bmw', 'audi', 'ferrari', 'mercedes', 'lamborghini', 'bugatti','mclaren']
 opcao = st.sidebar.selectbox('escolha o carro que foi alugado ou comprado', carros)
 
 st.image(f'{opcao}.png')
